f603f3c2d136f9b05ba19c3b72c16b44f308797d
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-03-28 10:51:38 -0500

    added the add task functionality and local storage for tasks

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const addBtn = document.querySelector(".add-task");
 const clearBtn = document.querySelector("#cancel");
 const submitBtn = document.querySelector("#submit");
+const form = document.querySelector('#myForm');
 
+let nameInput = document.querySelector('#task-name');
+let importanceImput = document.querySelector('#importance');
+let categoryInput = document.querySelector('#category');
+let dateInput = document.querySelector('#date');
+
+let table = document.querySelector('#tasks-table');
+
+let idCounter = 0;
+
+// functions //
 
 
 
@@ -11,17 +22,52 @@ const submitBtn = document.querySelector("#submit");
 // buttons //
 // opens the form
 addBtn.addEventListener("click", () => {
-    const form = document.querySelector("#myForm");
     form.style.display = 'block';
 });
 
 // clears the form and closes it
 clearBtn.addEventListener("click", () => {
-    const form = document.querySelector("#myForm");
     form.style.display = 'none';
 });
 
 // submitting the form with the details and then create the buttons to edit the form when interacting
-submitBtn.addEventListener("click", () => {
-    
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    idCounter += 1
+
+    let name = nameInput.value;
+    let importance = importanceImput.value;
+    let category = categoryInput.value;
+    let date = dateInput.value;
+
+    let storeTask = {
+        "id": idCounter,
+        "name": name,
+        "importance": importance,
+        "category": category,
+        "date": date
+    }
+
+    // adds task to local storage
+    localStorage.setItem(
+        "task", JSON.stringify(storeTask)
+    );
+
+    let template = `
+        <tr>
+        <td>${idCounter}</td>
+        <td>${name}</td>
+        <td>${importance}</td>
+        <td>${category}</td>
+        <td>${date}</td>
+        <td>
+            <button id="edit">Edit Task</button>
+            <button id="remove">Remove Task</button>
+        </td>
+    `;
+
+    table.innerHTML += template;
+
+    form.style.display = "none";
 });
+
