31f3d25934f984bcf8d9b4255a537fb575d5d093
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-04-04 11:54:42 -0500

    added remove functionality and working on editing

--- main.js
+++ main.js
@@ -4,20 +4,13 @@ const submitBtn = document.querySelector("#submit");
 const form = document.querySelector('#myForm');
 
 let nameInput = document.querySelector('#task-name');
-let importanceImput = document.querySelector('#importance');
+let importanceInput = document.querySelector('#importance');
 let categoryInput = document.querySelector('#category');
 let dateInput = document.querySelector('#date');
 
 let table = document.querySelector('#tasks-table');
-
+let taskId = null;
 let idCounter = 0;
-
-// functions //
-
-
-
-
-
 
 // buttons //
 // opens the form
@@ -36,7 +29,7 @@ submitBtn.addEventListener("click", (event) => {
     idCounter += 1
 
     let name = nameInput.value;
-    let importance = importanceImput.value;
+    let importance = importanceInput.value;
     let category = categoryInput.value;
     let date = dateInput.value;
 
@@ -55,15 +48,16 @@ submitBtn.addEventListener("click", (event) => {
 
     let template = `
         <tr>
-        <td>${idCounter}</td>
-        <td>${name}</td>
-        <td>${importance}</td>
-        <td>${category}</td>
-        <td>${date}</td>
-        <td>
-            <button id="edit">Edit Task</button>
-            <button id="remove">Remove Task</button>
-        </td>
+            <td>${idCounter}</td>
+            <td>${name}</td>
+            <td>${importance}</td>
+            <td>${category}</td>
+            <td>${date}</td>
+            <td>
+                <button class="edit" data-id="${idCounter}">Edit Task</button>
+                <button class="remove" data-id="${idCounter}">Remove Task</button>
+            </td>
+        </tr>
     `;
 
     table.innerHTML += template;
@@ -71,3 +65,24 @@ submitBtn.addEventListener("click", (event) => {
     form.style.display = "none";
 });
 
+table.addEventListener("click", function (event) {
+    if (event.target.classList.contains("edit")) {
+        taskId = event.target.dataset.id;
+        form.style.display = 'block';
+
+        const row = event.target.closest('tr');
+        const cells = row.querySelectorAll('td');
+
+        nameInput.value = cells[1].textContent;
+        importanceInput.value = cells[2].textContent;
+        categoryInput.value = cells[3].textContent;
+        dateInput.value = cells[4].textContent;
+    };
+
+    if (event.target.classList.contains("remove")) {
+        const row = event.target.closest('tr');
+        if (row) {
+            row.remove();
+        }
+    };
+})
